13227d9445b69f935d3fed649f1c795a5734863e
hirokyammt <hirokyammt@gmail.com>
2023-05-13 11:39:41 +0900

    Add chat UI without openai config

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,56 @@
-import { demos } from '#/lib/demos';
-import Link from 'next/link';
+'use client';
+
+import React from 'react';
 
 export default function Page() {
+  // States
+  const [value, setValue] = React.useState<string>("")
+  const [messages, setMessages] = React.useState<string[]>([])
+
+  const handleInput = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value)
+    },
+    []
+  );
+
+  const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setMessages([...messages, value]);
+      setValue('');
+      // const chatHistory = [...messages, { role: "user", content: value }]
+      // const response = await fetch("/api/openAIChat", {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify({ messages: chatHistory }),
+      // })
+      // const data = await response.json()
+      // setMessages([...messages, data.result.choices[0].message.content])
+    }
+  };
+
   return (
     <div className="space-y-8">
-      <h1 className="text-xl font-medium text-gray-300">Examples</h1>
+      <h1 className="text-xl font-medium text-gray-300">Please type your prompt</h1>
 
       <div className="space-y-10 text-white">
-        {demos.map((section) => {
-          return (
-            <div key={section.name} className="space-y-5">
-              <div className="text-xs font-semibold uppercase tracking-wider text-gray-400">
-                {section.name}
-              </div>
+        <input 
+          className='w-full rounded p-2 border-white'
+          placeholder='Type here'
+          value={value}
+          onChange={handleInput}
+          onKeyDown={handleKeyDown}
+        />
 
-              <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
-                {section.items.map((item) => {
-                  return (
-                    <Link
-                      href={`/${item.slug}`}
-                      key={item.name}
-                      className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800"
-                    >
-                      <div className="font-medium text-gray-200 group-hover:text-gray-50">
-                        {item.name}
-                      </div>
-
-                      {item.description ? (
-                        <div className="line-clamp-3 text-sm text-gray-400 group-hover:text-gray-300">
-                          {item.description}
-                        </div>
-                      ) : null}
-                    </Link>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
+      </div>
+      <div className='textarea'>
+        {messages.map((message, index) => (
+          <div key={index} className='mb-2'>
+            {message}
+          </div>
+        ))}
       </div>
     </div>
   );
